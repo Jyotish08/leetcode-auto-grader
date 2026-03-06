f2330346173d38fc2d295c919e8b6304b28bf28f
Jyotish Pabbisetti <jyotishpabbisetti2025@gmail.com>
2026-03-06 17:55:22 +0530

    Implement save_result function for CSV output day 5
    Add function to save results to a CSV file.

--- storage/excel_writer.py
+++ storage/excel_writer.py
@@ -1,1 +1,10 @@
-print("Excel storage module")
+import csv
+from datetime import datetime
+def save_result(problem,score):
+  file_name="result.csv"
+  date= datetime.now()strftime("%Y-%m-%d")
+  row=[date,problem,score]
+  with open(file_name,"a",newline="") as file:
+    writer=csv.writer(file)
+    writer.writerow(row)
+  print("result saved to",file_name)
