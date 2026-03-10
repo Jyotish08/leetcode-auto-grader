149dffb128daf1aced62d95df0b2ef74c005be61
Jyotish Pabbisetti <jyotishpabbisetti2025@gmail.com>
2026-03-10 19:20:16 +0530

    Add function to get Codeforces submissions
    Fetches the latest Codeforces submission data for a given user.

--- data_sources/codeforces_fetcher.py
+++ data_sources/codeforces_fetcher.py
@@ -1,1 +1,20 @@
+import requests
+def get_codeforces_submissions(jyotishpabbisetti2025):
+  url=f"https://codeforces.com/api/user.status?handle=jyotishpabbisetti2025"
+  response = requests.get(url)
+  data = response.json()
+  latest =data["result"][0]
+  submission_data={
+    "problem_name":latest["problem"],
+    "contest_id":latest["contestID"],
+    "submission_id":latest["id"],
+    "language":latest["programmingLanguage"],
+    "verdict":latest["verdict"]
+  }
+  return submisson_data
+
+if __name__ == "__main__":
+  sub = get_latest_codeforces_submission("jyotishpabbisetti2025")
+  print(sub)
+  
 
